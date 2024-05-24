ef020fcc0a24fa66398a8b3f4e154dc6e5e97296
bretmer <condorihuamanbrink05@gmail.com>
2024-05-23 20:14:58 -0500

    guardo mi archivo proyecto main.py

--- CONTROLES_FLUJO/Proyecto/main.py
+++ CONTROLES_FLUJO/Proyecto/main.py
@@ -1,33 +1,41 @@
-grass_reservado=[]
+horarios_disponibles=[ " 5:00 - 6:00 PM", " 7:00 - 8:00 PM", " 8:00 - 9:00 PM"," 11:00 - 12:00 PM"]
+reservacion:list=[]
 while True:
     print("Bienvenido al menu de grass sintetico")
     print("1. Horarios disponibles")
     print("2. Reservacion de grass")
-    print("3. Pago por alquiler")
+    print("3. pago por la reserva del grass")
     print("4. Verificar el alquiler")
-    opcion=int(input("ingrese una opcion del menu: "))
+    print("5. salir")
+    opcion=int(input("ingrese una opcion del menu grass sintetico: "))
     if opcion == 1:
-        grass_reservado.append(input('''Estos son los horarios disponible
-        1. Lunes
-         7:00 - 8:00
-         9:00 - 10:00
-        2. Martes
-         4:00 - 5:00
-         7:00 - 8:00
-        3. viernes
-         5:00 - 6:00
-         8:00 - 9:00
-              '''))
+        for i, horarios_disponibles in enumerate(horarios_disponibles,1):
+            print(f"{i}. {horarios_disponibles}")
     elif opcion == 2:
-        reservacion_grass=grass_reservado.pop(int(input("ingrese el indice de los horarios disponibles: ")) -1)
-        print(f"ha reservado el grass {reservacion_grass}")
+        indice=int(input("ingrese el indice del horario que desea reservar: "))
+        if indice<1 or indice > len(horarios_disponibles):
+            print("El horario no es valido")
+        else:
+            horario=horarios_disponibles.pop(indice -1)
+            reservacion.append(horario)
+            print(f"el horario {horario} se ha reservado con exito")
     elif opcion == 3:
-        pago_alquiler:int=35
-        print(f"el pago por el alquiler del grass es de s/{pago_alquiler}")
+        if not reservacion:
+            print("no tiene reservas actuales")
+        else:
+            indice=int(input("ingrese el indice del horario que va a pagar: "))
+            costo_alquiler=30
+            pago_alquiler=horarios_disponibles.append(int(input("El pago es de/30 por el alquiler...  ingresa el monto: ")))
+            if pago_alquiler==costo_alquiler:
+                print("su pago ha sido correcto")
+            else:
+                print("el pago no es el correcto")
     elif opcion == 4:
-        print("su reservacion de grass")
-        for i in enumerate(grass_reservado,1):
-            print(f"{reservacion_grass} y el pago del alquiler es s/.{pago_alquiler}")
-
-
-
+        for i,in enumerate(reservacion):
+            print(f"la reservacion {reservacion} ha sido cancelado con {pago_alquiler} correctamente")
+    elif opcion == 5:
+        break
+    else:
+        print("codigo invalido. Intenta de nuevo")
+        
+        
