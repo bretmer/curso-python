2d52ba67a5187786f1d92d151d245c5df6c9e90c
bretmer <condorihuamanbrink05@gmail.com>
2024-05-29 14:45:53 -0500

    solucione el ejercicio 2 de homework.py

--- TIPOS_DATOS_ESTRUCTURADOS/homework.py
+++ TIPOS_DATOS_ESTRUCTURADOS/homework.py
@@ -1,4 +1,5 @@
 # Ejercicio 1:
+
 # crear una lista de 5 alumnos cada alumno almacenaremos su nombre apellido y edad
 # insertrar al final de la listalos datos de antony
 # eliminar de la lista si existe los datos de abel
@@ -42,3 +43,40 @@ indice=lista_alumnos.index({
     "edad":25
 })
 print(lista_alumnos[indice])
+
+# Ejercicio 2
+
+# crear una lista con 4 diccionarios donde tendrán los datos de sus mascotas (nombre, edad, sexo, raza).
+# mostrar la lista con los 4 diccionarios
+# editar el 3er registro y cambiarle la edad sin modificar la lista original
+# mostrar la lista original y luego la lista con el 3er registro modificado
+lista_mascotas=[
+    {
+        "nombre":"bobi",
+        "edad":3,
+        "sexo":"macho",
+        "raza":"pastor aleman"
+    },
+    {
+        "nombre":"doky",
+        "edad":4,
+        "sexo":"macho",
+        "raza":"rottweiler"
+    },
+    {
+        "nombre":"luna",
+        "edad":2,
+        "sexo":"hembra",
+        "raza":"chihuahua"
+    },
+    {
+        "nombre":"zeus",
+        "edad":5,
+        "sexo":"macho",
+        "raza":"bóxer"
+    }
+]
+print(lista_mascotas)
+copia_lista_mascotas=lista_mascotas.copy()
+copia_lista_mascotas[2]["edad"]=4
+print(copia_lista_mascotas)
