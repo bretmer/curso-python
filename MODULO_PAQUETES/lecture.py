9df41b093b0d21d3a1ad02dc12a611b2705624f3
bretmer <condorihuamanbrink05@gmail.com>
2024-07-12 10:25:47 -0500

    actualizo mi archivo modulos

---
+++ MODULO_PAQUETES/lecture.py
@@ -0,0 +1,17 @@
+# """practicando pylit"""
+# nombre_alumno:str="miguel"
+
+# def suma(a:int,b:int)->int:
+#     """funcion para sumar dos numeros"""
+#     return a+b
+# print(suma(4,7))
+
+# def resta(a,b):
+#     """funcion para restar dos numeros"""
+#     return a-b
+# importacion directa
+import operaciones
+suma:int=operaciones.suma(7,8)
+mensaje:str=operaciones.mensaje
+print(suma)
+
---
+++ MODULO_PAQUETES/operaciones.py
@@ -0,0 +1,11 @@
+"""Modulo de operaciones aritmetricas"""
+def suma(a,b):
+    """funcion para sumar"""
+    return a+b
+def resta(a,b):
+    """funncion para resta"""
+    return a-b
+def producto(a,b):
+    """funcion de producto"""
+    return a*b
+mensaje:str="hola"
