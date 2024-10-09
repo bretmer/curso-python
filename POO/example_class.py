fb85b70791e2c19972ea3b0280dbd22def2fe2ee
bretmer <condorihuamanbrink05@gmail.com>
2024-10-09 15:29:13 -0500

    ultima clase

--- POO/example_class.py
+++ POO/example_class.py
@@ -1,35 +1,40 @@
 class Banco:
-    def __init__(self,name,lastname,cui,acount,amount):
-        self.name=name
-        self.lastname=lastname
-        self.cui=cui
-        self.acount=acount
-        self.amount=amount
-    def deposit(self,amount):
-        self.amount+=amount
-        
-    def remove_cash(self,amount):
+    def __init__(self, name, lastname, cui, account, amount):
+        self.name = name
+        self.lastname = lastname
+        self.cui = cui
+        self.account = account
+        self.amount = amount
+        self.transaction_history = []
+
+    def deposit(self, amount):
+        self.amount += amount
+        self.transaction_history.append(f"Depositaste: s/.{amount}") 
+
+    def remove_cash(self, amount):
         if amount > self.amount:
-            #este mensaje debe ser manejado por otro metodo
-            return "no cuentas con saldo suficiente"
-        self.amount-=amount
-        
-    def status_acount(self):
-        #este mensaje deberia mostrar el historial de depositos y retiros
-        response=f"""
-        ------BIENVENIDO AL BANCO "TEPINCHA Y ESTAFA"------
-        Cliente: {self.name}, {self.lastname}  NroCuenta:{self.acount}.
-        En estos momentos tienes un saldo de : s/.{self.amount},
-        fin del voucher:
-        ___________________________________________________________________
+            return "No cuentas con saldo suficiente"
+        self.amount -= amount
+        self.transaction_history.append(f"Retiraste: s/.{amount}")  
+
+    def status_account(self):
+        response = f"""
+        ------ BIENVENIDO AL BANCO "TEPINCHA Y ESTAFA" ------
+        Cliente: {self.name}, {self.lastname}  NroCuenta: {self.account}.
+        En estos momentos tienes un saldo de: s/.{self.amount},
+        Historial de transacciones:
         """
+        for transaction in self.transaction_history:
+            response += f"{transaction}\n"
+        response += "Fin del voucher:\n"
+        response += "___________________________________________________________________"
         return response
 
-cliente_miguel=Banco("Miguelito","Barraza",784512963,4545,30)
-print(cliente_miguel.status_acount())
-cliente_miguel.deposit(100)
-print(cliente_miguel.status_acount())
-cliente_miguel.remove_cash(80)
-print(cliente_miguel.status_acount())
-cliente_miguel.remove_cash(80)
-print(cliente_miguel.status_acount())
+# Example 
+cliente_miguel = Banco("Miguelito", "Barraza", 784512963, 4545, 30)
+cliente_miguel.deposit(200)
+cliente_miguel.remove_cash(100)
+print(cliente_miguel.status_account())
+cliente_miguel.deposit(300)
+cliente_miguel.remove_cash(30)
+print(cliente_miguel.status_account())
